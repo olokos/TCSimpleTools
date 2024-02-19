452d8331e5f1eecd3d2cf546c9dfe33b636c565a
olokos <1755581+olokos@users.noreply.github.com>
2024-02-19 18:10:26 +0100

    Fix Win10 crash bug, implement error handling, logging and catch exceptions
    This commit:
    - Renames the first textbox variable to more logical
    - Makes all textfields selectable, for easy copy/paste from the app into other places
    - Handles the 4 values independently, in case one of them is missing, it will log such error to the log file and properly show NOT FOUND, previously on Win10 one of the values was missing as compared to Win11, which resulted in crashing the app on start.
    This approach allows to show all of the existing ones regardless how many of them we have and in what combinations, could be done cleaner though.
    - Adds basic logging to a file, appending each line, due to UWP difficulties, we rely on the dir that UWP app picks itself, as it's always available
    - Adds a button which opens up the log file, so somebody could check the log file themselves without browsing into the folder manually, if the app opens.

--- TCSimpleTools/MainPage.xaml.cs
+++ TCSimpleTools/MainPage.xaml.cs
@@ -1,4 +1,11 @@
-﻿using Windows.System.Profile;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.System;
+using Windows.System.Profile;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 //Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x415
@@ -20,26 +27,131 @@ namespace TCSimpleTools
             GetWindowsVersion();
         }
 
-        private void GetWindowsVersion()
+        private async void GetWindowsVersion()
         {
-            var analyticsInfo = AnalyticsInfo.VersionInfo;
-            DeviceFamily = analyticsInfo.DeviceFamily;
-            DeviceFamilyVersion = analyticsInfo.DeviceFamilyVersion;
-            ProductName = analyticsInfo.ProductName;
+            // Try to read all 4 variables, each of them might not exist
+            try
+            {
+                var analyticsInfo = AnalyticsInfo.VersionInfo;
+                DeviceFamily = analyticsInfo.DeviceFamily;
+                //await LogToFile("Device Family set successfully: " + DeviceFamily);
+            }
+            catch (Exception ex)
+            {
+                DeviceFamily = "NOT FOUND";
+                await LogErrorToFile("Error setting Device Family: " + ex.Message);
+            }
 
-            // Parsing the version information
-            ulong v = ulong.Parse(DeviceFamilyVersion);
-            ulong v1 = (v & 0xFFFF000000000000L) >> 48;
-            ulong v2 = (v & 0x0000FFFF00000000L) >> 32;
-            ulong v3 = (v & 0x00000000FFFF0000L) >> 16;
-            ulong v4 = v & 0x000000000000FFFFL;
-            string version = $"{v1}.{v2}.{v3}.{v4}";
+            try
+            {
+                var analyticsInfo = AnalyticsInfo.VersionInfo;
+                DeviceFamilyVersion = analyticsInfo.DeviceFamilyVersion;
+                //await LogToFile("Device Family Version set successfully: " + DeviceFamilyVersion);
+            }
+            catch (Exception ex)
+            {
+                DeviceFamilyVersion = "NOT FOUND";
+                await LogErrorToFile("Error setting Device Family Version: " + ex.Message);
+            }
 
-            // Set values to the TextBoxes
-            txtBlock.Text = $"Windows Version: {version}";
-            txtDeviceFamily.Text = $"Device Family: {DeviceFamily}";
-            txtDeviceFamilyVersion.Text = $"Device Family Version: {DeviceFamilyVersion}";
-            txtProductName.Text = $"Product Name: {ProductName}";
+            try
+            {
+                var analyticsInfo = AnalyticsInfo.VersionInfo;
+                ProductName = analyticsInfo.ProductName;
+                //await LogToFile("Product Name set successfully: " + ProductName);
+            }
+            catch (Exception ex)
+            {
+                ProductName = "NOT FOUND";
+                await LogErrorToFile("Error setting Product Name: " + ex.Message);
+            }
+
+            try
+            {
+                string version = "NOT FOUND";
+                if (DeviceFamilyVersion != "NOT FOUND")
+                {
+                    // Parsing the version information
+                    ulong v = ulong.Parse(DeviceFamilyVersion);
+                    ulong v1 = (v & 0xFFFF000000000000L) >> 48;
+                    ulong v2 = (v & 0x0000FFFF00000000L) >> 32;
+                    ulong v3 = (v & 0x00000000FFFF0000L) >> 16;
+                    ulong v4 = v & 0x000000000000FFFFL;
+                    version = $"{v1}.{v2}.{v3}.{v4}";
+                }
+
+                // Set values to the TextBoxes
+                txtWindowsVersion.Text = $"Windows Version: {version}";
+                txtWindowsVersion.IsTextSelectionEnabled = true;
+
+                txtDeviceFamily.Text = $"Device Family: {DeviceFamily}";
+                txtDeviceFamily.IsTextSelectionEnabled = true;
+
+                txtDeviceFamilyVersion.Text = $"Device Family Version: {DeviceFamilyVersion}";
+                txtDeviceFamilyVersion.IsTextSelectionEnabled = true;
+
+                txtProductName.Text = $"Product Name: {ProductName}";
+                txtProductName.IsTextSelectionEnabled = true;
+
+                // Logging variables
+                await LogToFile(txtWindowsVersion.Text + "\n" + txtDeviceFamily.Text + "\n" + txtDeviceFamilyVersion.Text + "\n" + txtProductName.Text);
+            }
+            catch (Exception ex)
+            {
+                await LogErrorToFile("Error parsing or setting version information: " + ex.Message);
+            }
         }
+
+        private async Task LogErrorToFile(string error)
+        {
+            StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+            StorageFile logFile = await localFolder.CreateFileAsync("tc-tools-log.txt", CreationCollisionOption.OpenIfExists);
+            string AppName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+            string AppVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            using (StreamWriter writer = File.AppendText(logFile.Path))
+            {
+                await writer.WriteLineAsync($"\n[Error] {DateTime.Now:dd.MM.yyyy HH:mm:ss} {AppName} {AppVersion}:\n{error}");
+            }
+        }
+
+        private async Task LogToFile(string message)
+        {
+            StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+            StorageFile logFile = await localFolder.CreateFileAsync("tc-tools-log.txt", CreationCollisionOption.OpenIfExists);
+            string AppName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+            string AppVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            using (StreamWriter writer = File.AppendText(logFile.Path))
+            {
+                await writer.WriteLineAsync($"\n{DateTime.Now:dd.MM.yyyy HH:mm:ss} {AppName} {AppVersion}:\n{message}");
+            }
+        }
+
+        private async void OpenLogs_Click(object sender, RoutedEventArgs e)
+        {
+            StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+            StorageFile logFile = await localFolder.CreateFileAsync("tc-tools-log.txt", CreationCollisionOption.OpenIfExists);
+
+            try
+            {
+                // Launch the default app associated with the file
+                await Launcher.LaunchFileAsync(logFile);
+            }
+            catch (Exception ex) when (ex.HResult == -2147220991)
+            {
+                // File type not associated with any app
+                var messageDialog = new MessageDialog("You don't have any default app set in Windows to open .txt files!");
+                await messageDialog.ShowAsync();
+                Application.Current.Exit();
+            }
+            catch (Exception ex)
+            {
+                // Other exceptions
+                var messageDialog = new MessageDialog("An error occurred: " + ex.Message);
+                await messageDialog.ShowAsync();
+            }
+        }
+
     }
 }
